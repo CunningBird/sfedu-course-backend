30268cb57b35307c59079f003962aea7177706a7
CunningBird <68229355+CunningBird@users.noreply.github.com>
2022-11-19 13:46:13 +0300

    initial commit

---
+++ src/test/kotlin/ru/sfedu/course/backend/database/DatabaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.sfedu.course.backend.database
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
